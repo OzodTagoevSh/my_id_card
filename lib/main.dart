a0876ffe4b74f78d2297bc01259ba9ab454c68b8
ozodtagoev <ozod.tagoev@gmail.com>
2021-06-12 13:39:17 +0500

    Column added

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,35 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('My ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[800],
         elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey[400],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Ozod Tagoev',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 1.0
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
